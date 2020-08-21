5ccbe5978eb425c6fd0921e1b809bda37cd4afee
Luis Dubrez <luis.dubrez@gmail.com>
2020-08-21 10:13:30 -0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  title = 'sparkTest';
+  boxes = [
+    'box1', 'box3', 'box3', 'box4',
+  ];
+  pos: number;
+  posAnt: number;
+  showBallVisible = false;
+  showBallStatus: string;
+  ngOnInit(): void {
+    this.showBall();
+  }
+  showBall(i = 0) {
+    this.posAnt = this.pos;
+    this.showBallVisible = true;
+    this.pos = i;
+  }
+}
